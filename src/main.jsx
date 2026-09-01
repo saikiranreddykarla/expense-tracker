052dc06f0ea9c1a82b5466d6f4f10465f32d52d4
saikiranreddykarla <saikiranreddykarla7@gmail.com>
2026-09-01 15:01:49 +0530

    Use hash routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './store/store'
 import './index.css'
@@ -32,9 +32,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppErrorBoundary>
       <Provider store={store}>
-        <BrowserRouter basename="/expense-tracker">
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </AppErrorBoundary>
   </React.StrictMode>,
